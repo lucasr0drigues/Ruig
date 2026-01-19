a907f807b204d748cb8378d4a8051d60fd601a39
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2026-01-18 23:25:15 -0300

    Add Strava Interfaces

---
+++ src/Ruig.Application/Common/Interfaces/Strava/IStravaApiClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ruig.Application.Common.Interfaces.Strava
+{
+    public interface IStravaApiClient
+    {
+    }
+}
