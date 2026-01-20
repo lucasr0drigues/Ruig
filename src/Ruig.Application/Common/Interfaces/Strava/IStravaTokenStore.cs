7cf0f1b1799707d06ae889a919f804917a356688
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2026-01-20 02:39:06 -0300

    Define IStravaTokenStore

--- src/Ruig.Application/Common/Interfaces/Strava/IStravaTokenStore.cs
+++ src/Ruig.Application/Common/Interfaces/Strava/IStravaTokenStore.cs
@@ -6,5 +6,7 @@ namespace Ruig.Application.Common.Interfaces.Strava
 {
     public interface IStravaTokenStore
     {
+        Task SaveOrUpdateAsync(Guid athleteId, long stravaAthleteId, string accessToken, string refreshToken, DateTimeOffset expiresAtUtc, string scope, CancellationToken cancellationToken);
+        Task<string?> GetAccessTokenAsync(Guid athleteId, CancellationToken cancellationToken);
     }
 }
