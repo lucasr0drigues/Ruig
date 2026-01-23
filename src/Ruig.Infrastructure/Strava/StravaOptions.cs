03c489105611203740953f5bfdaf616feac4b01d
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2026-01-22 23:42:48 -0300

    Add StravaToken

---
+++ src/Ruig.Infrastructure/Strava/StravaOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ruig.Infrastructure.Strava
+{
+    public sealed class StravaOptions
+    {
+        public string ClientId { get; init; } = default!;
+        public string ClientSecret { get; init; } = default!;
+        public string RedirectUri { get; init; } = default!;
+        public string AuthorizeBaseUrl { get; init; } = default!;
+        public string TokenUrl { get; init; } = default!;
+        public string ApiBaseUrl { get; init; } = default!;
+    }
+}
---
+++ src/Ruig.Infrastructure/Strava/StravaToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ruig.Infrastructure.Strava
+{
+    public sealed class StravaToken
+    {
+        public Guid Id { get; set; }
+        public Guid AthleteId { get; set; }
+        public long StravaAthleteId { get; set; }
+        public string AccessToken { get; set; } = default!;
+        public string RefreshToken { get; set; } = default!;
+        public DateTimeOffset ExpiresAtUtc { get; set; }
+        public string Scope { get; set; } = string.Empty;
+    }
+}
